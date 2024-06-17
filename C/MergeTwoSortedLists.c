7f54a10835ad36c31185a3c04e0e1cc9a5bbbb2b
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-06-17 20:46:20 +0200

    Create MergeTwoSortedLists.c

---
+++ C/MergeTwoSortedLists.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
+    dummy->val = 0;
+    struct ListNode* current = dummy;
+
+    while (list1 && list2) {
+        if (list1->val > list2->val) {
+            current->next = list2;
+            list2 = list2->next;
+        } 
+        else {
+            current->next = list1;
+            list1 = list1->next;
+        }
+        current = current->next;
+    }
+
+    current->next = list1 ? list1 : list2;
+
+    return dummy->next;     
+}
