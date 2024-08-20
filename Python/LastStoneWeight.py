8632fdc11b78a5a80a5e4f2ec74037dc63b91004
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-08-20 14:37:56 +0200

    Create LastStoneWeight.py

---
+++ Python/LastStoneWeight.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        stones = [-stone for stone in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = -heapq.heappop(stones)
+            second = -heapq.heappop(stones)
+
+            if first != second:
+                heapq.heappush(stones, -(first - second))
+
+        return -stones[0] if stones else 0
