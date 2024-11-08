896db2b9577e6fc47b63d0f91537045b216b92f2
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2024-11-08 23:22:28 +0100

    Create MaxXORforEachQuery.py

---
+++ Python/MaxXORforEachQuery.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def getMaximumXor(self, nums, maximumBit):
+        n = len(nums)
+        xorr = nums[0]
+        max_xor = (1 << maximumBit) - 1
+        
+        for i in range(1, n):
+            xorr ^= nums[i]
+        
+        ans = []
+        for i in range(n):
+            ans.append(xorr ^ max_xor)
+            xorr ^= nums[n - 1 - i]
+        
+        return ans
+        
