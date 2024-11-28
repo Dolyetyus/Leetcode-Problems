abadd8d6d3074172ebff05b234e6c369ad5c6bb8
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2024-11-29 00:46:56 +0100

    Create TypeOfTriangle.c

---
+++ C/TypeOfTriangle.c
@@ -0,0 +1,6 @@
+char* triangleType(int* nums, int numsSize) {
+    if (nums[0]+nums[1]<=nums[2] || nums[0]+nums[2]<=nums[1] || nums[1]+nums[2]<=nums[0]) return "none";
+    if (nums[0]==nums[1] && nums[1]==nums[2]) return "equilateral";
+    if (nums[0]==nums[1] || nums[1]==nums[2] || nums[0]==nums[2]) return "isosceles";
+    return "scalene";
+}
