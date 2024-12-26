b146736cbbcf08da60dd9674dc0ccd3f5aa340f9
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2024-12-26 23:49:01 +0100

    Create TargetSum.c

---
+++ Python/TargetSum.c
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findTargetSumWays(self, nums, target):
+        dp = defaultdict(int)
+        dp[0] = 1
+
+        for num in nums:
+            next_dp = defaultdict(int)
+            for current_sum, count in dp.items():
+                next_dp[current_sum + num] += count
+                next_dp[current_sum - num] += count
+            dp = next_dp
+
+        return dp[target]T
