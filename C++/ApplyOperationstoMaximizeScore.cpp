b82087c7010af9039a60f27c3f27f63af297e03a
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2025-03-29 13:28:21 +0100

    Create ApplyOperationstoMaximizeScore.cpp

---
+++ C++/ApplyOperationstoMaximizeScore.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    int mod=1e9+7;
+
+    int countprime(int n) {
+        int cnt=0;
+
+        if(n%2==0) {
+            cnt++;
+            while (n % 2 == 0) {
+                n = n/2;
+            }
+        }
+
+        for (int i = 3; i <= sqrt(n); i = i+2) {
+            if(n%i==0) {
+                cnt++;
+                while (n % i == 0)  n = n/i;
+            }
+        }
+    
+        if (n > 2) cnt++;
+
+        return cnt;
+    }
+    
+    int modPow(int x, int y) {
+        if (y == 0) return 1;
+
+        long p = modPow(x, y / 2) % mod;
+        p = (p * p) % mod;
+
+        return y % 2 ? (p * x) % mod : p;
+    }
+
+    int maximumScore(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> prime(n);
+
+        for(int i=0;i<n;i++) {
+            prime[i]=countprime(nums[i]);
+        }
+
+        priority_queue<pair<int,int>> pq;
+        for(int i=0;i<n;i++) pq.push({nums[i],i});
+
+        long long score=1;
+
+        while(k>0) {
+            int maxi=pq.top().first;
+            int ind=pq.top().second;
+            int i=ind-1;
+            pq.pop();
+            int maxcount=prime[ind];
+            int cnt1=0;
+            
+            while(ind<n && prime[ind]<=maxcount) {
+                cnt1++;
+                ind++;
+            }
+
+            long long cnt2=1;
+            while(i>=0  && prime[i]<maxcount) {
+                cnt2++;
+                i--;
+            }
+
+            long long take = 0;
+            if (cnt1*cnt2 > k) take = k;
+            else take = cnt1*cnt2;
+            
+            k-=take;
+            score=(score*modPow(maxi,take))%mod;
+
+            if(k==0) break;
+        }
+
+        return score;
+    }
+};
