ca9ce6d467e8f22cf08ab9ea889b4cbf9a2bd3a5
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2025-04-15 16:14:19 +0200

    Create CountGoodTripletsinanArray.py

---
+++ Python/CountGoodTripletsinanArray.py
@@ -0,0 +1,32 @@
+class Solution:
+    def goodTriplets(self, nums1, nums2):
+        value_to_index = {num: i for i, num in enumerate(nums1)}
+
+        sorted_indices = []
+        total_triplets = 0
+
+        for value in nums2:
+            index_in_nums1 = value_to_index[value]
+
+            left_count = self._binary_search_insert_position(sorted_indices, index_in_nums1)
+
+            right_count = (len(nums1) - 1 - index_in_nums1) - (len(sorted_indices) - left_count)
+
+            total_triplets += left_count * right_count
+
+            insert_pos = self._binary_search_insert_position(sorted_indices, index_in_nums1)
+            sorted_indices.insert(insert_pos, index_in_nums1)
+
+        return total_triplets
+
+    def _binary_search_insert_position(self, lst, target):
+        low, high = 0, len(lst)
+
+        while low < high:
+            mid = (low + high) // 2
+            if lst[mid] < target:
+                low = mid + 1
+            else:
+                high = mid
+
+        return low
