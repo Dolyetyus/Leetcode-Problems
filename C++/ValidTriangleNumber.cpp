9d85c7073f06b5955c2907372723a0c019333a17
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2025-09-26 16:54:22 +0200

    Create ValidTriangleNumber.cpp

---
+++ C++/ValidTriangleNumber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int ret = 0;
+
+        for (int k = n-1; k >= 2; --k) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (nums.at(i) + nums.at(j) > nums.at(k)) {
+                    ret += j-i;
+                    j--;
+                } 
+                else i++;
+            }
+        }
+        return ret;
+    }
+};
