2f3253e1daacaced4591f11388133fd20f2c8701
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2025-10-03 13:22:07 +0200

    TrappingRainWaterII.py

--- Python/TrappingRainWaterII.py
+++ Python/TrappingRainWaterII.py
@@ -6,7 +6,7 @@ class Solution(object):
         
         for i in range(m):
             for j in range(n):
-                if i == 0 or i == m - 1 or j == 0 or j == n - 1:
+                if i == 0 or i == m-1 or j == 0 or j == n-1:
                     heappush(heap, (heightMap[i][j], i, j))
                     visited[i][j] = True
 
