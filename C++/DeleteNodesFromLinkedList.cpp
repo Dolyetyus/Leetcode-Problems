5b59380a5ff29f13921741070afa6af52d145bfd
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2025-11-01 16:11:03 +0100

    Update DeleteNodesFromLinkedList.cpp

--- C++/DeleteNodesFromLinkedList.cpp
+++ C++/DeleteNodesFromLinkedList.cpp
@@ -1,15 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+    // much better solution lol
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> st;
+
+        for (int num: nums) {
+            st.insert(num);
+        }
+
+        ListNode* ret = head;
+        ListNode* prev = NULL;
+        while (head != NULL) {
+            if (st.contains(head->val)) {
+                if (prev == NULL) ret = head->next;
+                else prev->next = head->next;
+            }
+            else prev = head;
+            head = head->next;
+        }
+
+        return ret;
+    }
     /* Idk why I made my first approach extremely complicated
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
             std::set<int> hmap(nums.begin(), nums.end());
