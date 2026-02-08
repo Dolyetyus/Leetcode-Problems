ce5beb83a07f03f5c7e6ae788453e7c224c1c866
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2026-02-08 14:25:33 +0100

    Create BalancedBinaryTree.c

---
+++ C/BalancedBinaryTree.c
@@ -0,0 +1,17 @@
+int height(struct TreeNode* root) {
+    if (!root) return 0;
+
+    int lh = height(root->left);
+    if (lh == -1) return -1;
+
+    int rh = height(root->right);
+    if (rh == -1) return -1;
+
+    if (abs(lh - rh) > 1) return -1;
+
+    return 1 + (lh > rh ? lh : rh);
+}
+
+bool isBalanced(struct TreeNode* root) {
+    return height(root) != -1;
+}
