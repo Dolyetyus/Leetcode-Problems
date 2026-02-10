85dc92d0367260b5ce31612a23c94ce0e0c72bc9
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2026-02-10 23:53:57 +0100

    Create LongestBalancedSubarrayI.cpp

---
+++ C++/LongestBalancedSubarrayI.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int ret = 0;
+
+        for (int i = 0; i < n; i++) {
+            unordered_set<int> odd, even;
+            for (int j = i; j<n; j++) {
+                if (nums[j] % 2) odd.insert(nums[j]);
+                else even.insert(nums[j]);
+
+                if (odd.size() == even.size()) ret = max(ret, j-i+1);
+            }
+        }
+        return ret;
+    }
+};
