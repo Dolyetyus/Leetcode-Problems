7c5424f6e02fde96b5d6336a251fffdbdb4f5f5d
Dolyetyus <67073431+Dolyetyus@users.noreply.github.com>
2026-03-30 11:20:16 +0200

    Create CheckifStringsCanbeMadeEqualWithOperationsII.cpp

---
+++ C++/CheckifStringsCanbeMadeEqualWithOperationsII.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        map<char, int> even1, odd1, even2, odd2;
+        
+        for (int i = 0; i<s1.size(); i++) {
+            if (i % 2 == 0) {
+                even1[s1[i]]++;
+                even2[s2[i]]++;
+            } 
+            else {
+                odd1[s1[i]]++;
+                odd2[s2[i]]++;
+            }
+        }
+        
+        return even1 == even2 && odd1 == odd2;
+    }
+};
